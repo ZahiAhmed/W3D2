f80fe79f957e9ac862e69df071b259302887f5a9
ZahiAhmed <zahi.ahmed33@gmail.com>
2022-11-22 17:13:46 -0500

    txt file and board and tile class progress zahi drive, chak nav

--- Sudoku_Project/board.rb
+++ Sudoku_Project/board.rb
@@ -1,12 +1,20 @@
+require_relative 'tile.rb'
+
 class Board
 
     def self.from_file(file)
-        file_data = File.("sudoku.txt").readlines.map(&:chomp)
-        Array.new(9){Array.new(9 , Tile.new(0))}
+        array = []
+        File.readlines(file).each do |line|
+            subarray = []
+            chars = line.chomp.split("")
+            chars.each {|char| subarray << Tile.new(char)}
+            array << subarray
+        end
+        array
     end
 
     def initialize
-        
+        @grid = Board.from_file("sudoku.txt")
     end
 
     
--- Sudoku_Project/tile.rb
+++ Sudoku_Project/tile.rb
@@ -1,7 +1,33 @@
+require "colorize"
+
 class Tile
 
-    def initialize(value, given)
-        @value = value
+    attr_reader :value
 
+    def initialize(value)
+        @value = value
+    end
+
+    def value= (new_value)
+        if !self.given?
+            @value = new_value
+        end
+    end
+
+    def to_s
+        @value = "#{@value}"
+    end
+
+    def color 
+        if given?
+            self.colorize(:blue)
+        else
+            self.colorize(:red)
+        end
+    end
+
+    def given?
+        @value == 0
+    end
 
 end
