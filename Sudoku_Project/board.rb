17ebd623f5b4c111fe6455e96b63eabdd45315a9
chakhoic <chakhoichan@gmail.com>
2022-11-22 16:31:22 -0500

    chak drive zahi nav

--- Sudoku_Project/board.rb
+++ Sudoku_Project/board.rb
@@ -1,7 +1,8 @@
 class Board
 
-    def self.from_file
-
+    def self.from_file(file)
+        file_data = File.("sudoku.txt").readlines.map(&:chomp)
+        Array.new(9){Array.new(9 , Tile.new(0))}
     end
 
     def initialize
